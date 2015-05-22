d67884f73a20b7346f5190e081c3e5548afc44a0
flipturnapps <flipturnapps@gmail.com>
2015-05-22 09:47:48 +0800

    RoomGenerator creation.

---
+++ src/com/flipturnapps/quinton/worldgeneration/RoomGenerator.java
@@ -0,0 +1,13 @@
+package com.flipturnapps.quinton.worldgeneration;
+
+import com.flipturnapps.quinton.xmldata.Room;
+
+public class RoomGenerator
+{
+	private Room generateStartRoom()
+	{
+		Room startRoom = new Room();
+		//set as many sexy fields as you can here.
+		return startRoom;
+	}
+}
