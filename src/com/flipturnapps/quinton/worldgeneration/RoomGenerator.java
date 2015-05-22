53bd8dd04b38ab0945decc7d87fa0cfd460a04c1
flipturnapps <flipturnapps@gmail.com>
2015-05-22 11:58:35 +0800

    Fix of RoomGenerator.

--- src/com/flipturnapps/quinton/worldgeneration/RoomGenerator.java
+++ src/com/flipturnapps/quinton/worldgeneration/RoomGenerator.java
@@ -1,12 +1,16 @@
 package com.flipturnapps.quinton.worldgeneration;
 
+import com.flipturnapps.quinton.xmldata.DirectionConstraints;
+import com.flipturnapps.quinton.xmldata.ItemGen;
+import com.flipturnapps.quinton.xmldata.Location;
+import com.flipturnapps.quinton.xmldata.Region;
 import com.flipturnapps.quinton.xmldata.Room;
 
 public class RoomGenerator
 {
 	
 	
-	private Room generateStartRoom()
+	public Room generateStartRoom()
 	{
 		Room startRoom = new Room();
 		startRoom.setName("A Dark Forest");
@@ -14,11 +18,14 @@ public class RoomGenerator
 		startRoom.setStartNarration("You stand alone in a forest");
 		startRoom.setLocation(new Location(0,0,0));
 		startRoom.setId(0);
-		startRoom.setDirectionConstraints(new DirectionConstraints());
+		startRoom.setDirConstraints(new DirectionConstraints());
 		startRoom.setRegion(new Region("forest"));
-		startRoom.
+		ItemGen axe = new ItemGen();
+		//more info
+		startRoom.addItem(axe);
 		return startRoom;
 		
 		
 	}
 }
+
--- src/com/flipturnapps/quinton/xmldata/DirectionConstraints.java
+++ src/com/flipturnapps/quinton/xmldata/DirectionConstraints.java
@@ -1,5 +1,5 @@
 package com.flipturnapps.quinton.xmldata;
-import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
